911dca16e00e027fffd56befb36575836688d71d
cjq002 <chenjiaqing@Mac-mini-CJQ.local>
2015-02-07 16:59:48 +0800

    Initial Commit

---
+++ 自定义UIPickerViewTests/___UIPickerViewTests.m
@@ -0,0 +1,34 @@
+//
+//  ___UIPickerViewTests.m
+//  自定义UIPickerViewTests
+//
+//  Created by 陈家庆 on 15-2-7.
+//  Copyright (c) 2015年 shikee_Chan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ___UIPickerViewTests : XCTestCase
+
+@end
+
+@implementation ___UIPickerViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
